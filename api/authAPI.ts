0c8abed5511ffa017c452ef4a6c2eab66a481b6e
maksfourlife <maksfourlife@gmail.com>
2025-05-05 13:56:29 +0200

    add blink-sdk as a dependency, added script
    - now imports are from 'blink-sdk' not from '@/submodule/..'
    - call 'preinstall:blink-sdk' to install blink-sdk

--- api/authAPI.ts
+++ api/authAPI.ts
@@ -1,22 +1,23 @@
-import {BlinkClient, decryptSessionKeyPair, initSession} from "@/submodule/src";
+import { BlinkClient, decryptSessionKeyPair, initSession } from "blink-sdk";
 
 export const authAPI = {
-    async generateLink(): Promise<string> {
-        const {privateKey, url} = await initSession();
-        localStorage.setItem("privateKey", privateKey);
-        return url;
-    },
+  async generateLink(): Promise<string> {
+    const { privateKey, url } = await initSession();
+    localStorage.setItem("privateKey", privateKey);
+    return url;
+  },
 
-    async getUser(params?: string): Promise<BlinkClient> {
-        const keyPair = await decryptSessionKeyPair(localStorage.getItem("privateKey") || "", params || localStorage.getItem('params') || '');
-        const client =  BlinkClient.http(process.env.NEXT_PUBLIC_API_URL!, keyPair)
+  async getUser(params?: string): Promise<BlinkClient> {
+    const keyPair = await decryptSessionKeyPair(
+      localStorage.getItem("privateKey") || "",
+      params || localStorage.getItem("params") || ""
+    );
+    const client = BlinkClient.http(process.env.NEXT_PUBLIC_API_URL!, keyPair);
 
-        if(params) {
-            localStorage.setItem('params', params);
-        }
-
-        return client;
-
-
+    if (params) {
+      localStorage.setItem("params", params);
     }
-}
+
+    return client;
+  },
+};
--- api/tradeAPI.ts
+++ api/tradeAPI.ts
@@ -1,25 +1,28 @@
-import {TradesClient} from "@/submodule/src";
+import { TradesClient } from "blink-sdk";
 
 export const tradeAPI = {
-    async fetchTransactions(address: string){
-        const httpClient = TradesClient.http(`http://${process.env.NEXT_PUBLIC_TRADES_URL}`);
+  async fetchTransactions(address: string) {
+    const httpClient = TradesClient.http(
+      `http://${process.env.NEXT_PUBLIC_TRADES_URL}`
+    );
 
-        return await httpClient.getTrades({
-            amm: address,
-            limit: 10,
-        });
-    },
+    return await httpClient.getTrades({
+      amm: address,
+      limit: 10,
+    });
+  },
 
-    async fetchStatistics(address: string) {
-        const client = TradesClient.websocket(`ws://${process.env.NEXT_PUBLIC_TRADES_URL}`);
-        const total_data = await client.getTotals({
-            amm: address,
-            dir: 0
-        })
-        return total_data.map((total, index) => ({
-            id: index,
-            ...total
-        }));
-    }
-
-}
+  async fetchStatistics(address: string) {
+    const client = TradesClient.websocket(
+      `ws://${process.env.NEXT_PUBLIC_TRADES_URL}`
+    );
+    const total_data = await client.getTotals({
+      amm: address,
+      dir: 0,
+    });
+    return total_data.map((total, index) => ({
+      id: index,
+      ...total,
+    }));
+  },
+};
